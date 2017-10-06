f4f5343068817e4638ae2fa4c71e6fcfe9beaad4
Jake Howard <git@theorangeone.net>
2017-10-06 19:33:36 +0100

    Be loud!

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -9,7 +9,7 @@ git config --global user.name $CIRCLE_USERNAME
 
 echo -e "Starting deployment to Github Pages\n"
 # Using token clone gh-pages branch
-git clone --quiet --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
+git clone --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
 
 # Go into directory and copy data we're interested in to that directory
 cd $DEPLOY_DIR
@@ -18,6 +18,6 @@ rsync -rv --exclude=.git ../public/* .
 # Add, commit and push files
 git add -f .
 git commit -m "Deploy build $CIRCLE_BUILD_NUM [ci-skip]"
-git push -fq origin gh-pages
+git push -f origin gh-pages
 
 echo -e "Deployment completed.\n"

03e1c3344f23c742c133712d5725530ce2906497
Jake Howard <git@theorangeone.net>
2017-10-06 20:05:06 +0100

    Cleanup deploy script

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -7,17 +7,15 @@ DEPLOY_DIR=deploy
 git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
 git config --global user.name $CIRCLE_USERNAME
 
-echo -e "Starting deployment to Github Pages\n"
-# Using token clone gh-pages branch
-git clone --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
+echo "Starting deployment to Github Pages"
 
-# Go into directory and copy data we're interested in to that directory
+git clone -q --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
+
 cd $DEPLOY_DIR
 rsync -ar --delete ../public/* .
 
-# Add, commit and push files
 git add -f .
-git commit -m "Deploy build $CIRCLE_BUILD_NUM [ci-skip]"
-git push -f origin gh-pages
+git commit -m "Deploy build $CIRCLE_BUILD_NUM\n[ci skip]"
+git push -f
 
-echo -e "Deployment completed.\n"
+echo "Deployment completed."

e06f5a8e96f2d5f89430a8d2f3e0f1495191d9e0
Jake Howard <git@theorangeone.net>
2017-10-06 22:58:07 +0100

    Gen syntax highlighting stuff too

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,8 @@ rm -rf public/
 rm -rf static/build
 mkdir -p static/build/js static/build/css
 
+hugo gen chromastyles --style=monokai > static/src/scss/highlight.css
+
 node-sass static/src/scss/index.scss static/build/css/index.css --source-map-embed
 browserify static/src/js/index.js -o static/build/js/app.js
 

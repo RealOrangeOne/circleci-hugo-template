356783195c8a80e301d4e7df34c2321e35b40f04
Jake Howard <git@theorangeone.net>
2017-10-06 22:41:51 +0100

    Remove echos

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -8,8 +8,6 @@ git config --global push.default simple
 git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
 git config --global user.name $CIRCLE_USERNAME
 
-echo "Starting deployment to Github Pages"
-
 git clone -q --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
 
 cd $DEPLOY_DIR
@@ -18,5 +16,3 @@ rsync -arv --delete ../public/* .
 git add -f .
 git commit -m "Deploy build $CIRCLE_BUILD_NUM [ci skip]" || true
 git push -f
-
-echo "Deployment completed."

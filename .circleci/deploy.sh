c7ccebbb877aaa4d71b0ad56db442d08ae0b2f27
Jake Howard <git@theorangeone.net>
2017-10-06 20:16:21 +0100

    Use simple push method

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -4,6 +4,7 @@ set -e
 
 DEPLOY_DIR=deploy
 
+git config --global push.default simple
 git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
 git config --global user.name $CIRCLE_USERNAME
 

0b05ceb3950046501edceeb7a74d22d842363497
Jake Howard <git@theorangeone.net>
2017-10-06 22:54:46 +0100

    Add static build stuff

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+export PATH=node_modules/.bin:${PATH}
+
+rm -rf public/
+rm -rf static/build
+mkdir -p static/build/js static/build/css
+
+node-sass static/src/scss/index.scss static/build/css/index.css --source-map-embed
+browserify static/src/js/index.js -o static/build/js/app.js
+
+hugo -v
